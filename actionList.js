305e9021c35fec5f93b7ce3e37cb1dcc1cf25740
landbronx <alexander.landborn@gmail.com>
2024-05-03 10:23:44 +0200

    Add files via upload

---
+++ actionList.js
@@ -0,0 +1,26 @@
+const actionList = {
+  office: {
+    actionItems: [
+      'Action 1 for Office',
+      'Action 2 for Office',
+      // Add more action items as needed
+    ],
+  },
+  residential: {
+    actionItems: [
+      'Action 1 for Residential',
+      'Action 2 for Residential',
+      // Add more action items as needed
+    ],
+  },
+  steel: {
+    actionItems: [
+      'Action 1 for Stål',
+      'Action 2 for Stål',
+      // Add more action items as needed
+    ],
+  },
+  // Add more building types and structure types as needed
+};
+
+export default actionList;
---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ProjectForm from './ProjectForm'; // Import ProjectForm component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ProjectForm />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
